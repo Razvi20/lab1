4931e1d234114325874b1cd865912219d9263f82
Razvi20 <nicuopris11@gmail.com>
2022-10-08 16:05:57 +0300

    Add more line of code

--- src/First.java
+++ src/First.java
@@ -1,5 +1,10 @@
 public class First {
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
+        String name = "Razvan";
+        System.out.println(name);
+        System.out.println(4 + 6);
+
     }
 }
